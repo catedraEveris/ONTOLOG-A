637e04d175229da339248a3247996cf7dd2af97b
catedraEveris <40594466+catedraEveris@users.noreply.github.com>
2018-06-26 12:41:15 +0200

    Add files via upload

---
+++ Keras/vectorize_all_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+seq_len=10
+emb=data/glove.6B.300d.txt
+emb_dim=300
+
+java -jar vectorizer.jar -d ${emb_dim} -e ${emb} -s ${seq_len}
+java -jar vectorizer.jar -d ${emb_dim} -e ${emb} -s ${seq_len} -o data_test_a/ -i data/eng.testa.txt -l data/meta.yaml
+java -jar vectorizer.jar -d ${emb_dim} -e ${emb} -s ${seq_len} -o data_test_b/ -i data/eng.testb.txt -l data/meta.yaml
